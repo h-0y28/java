b588b844e8db6c4b7300f54e83edc3d821cb7a68
hyojoo <jhj080802@naver.com>
2024-09-18 21:38:06 +0900

    240918

---
+++ textbook/page124num8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class page124num8 {
+    public static void main(String[] args) {
+            System.out.println(factorial(5));
+    }
+    public static int factorial(int a) {
+        switch (a) {
+            case 1 -> {
+                return 1;
+            }
+            case 2 -> {
+                return 2;
+            }
+            case 3 -> {
+                return 3;
+            }
+            case 4 -> {
+                return 4;
+            }
+            case 5 -> {
+                return 5;
+            }
+            case 6 -> {
+                return 6;
+            }
+            case 7 -> {
+                return 7;
+            }
+
+        }
+        return 0;
+    }
+}
