60d86832834750116d7017b8edb953544115db3e
hyojoo <jhj080802@naver.com>
2024-09-18 21:38:02 +0900

    240918

---
+++ textbook/page123num6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class page123num6 {
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            System.out.print("철수 : ");
+            String c = in.nextLine();
+
+            System.out.print("영희 : ");
+            String y = in.nextLine();
+
+    }
+}
