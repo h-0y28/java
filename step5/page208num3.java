260fb3cb3249a1e4a60d0adfdcf60039f5bf3b0e
hyojoo <jhj080802@naver.com>
2024-10-04 19:42:11 +0900

    241004 - step 5

--- step5/page208num3.java
+++ step5/page208num3.java
@@ -1,6 +1,10 @@
-//public class page208num3 {
-//    public static void main(String[] args) {
-//        for(Direction d : Direction.values())
-//            System.out.print(d+" ");
-//    }
-//}
+public class page208num3 {
+    public static void main(String[] args) {
+        for(Direction d : Direction.values())
+            System.out.print(d+" ");
+    }
+}
+
+enum Direction {
+  동, 서, 남, 북
+}
--- step5/page210num8.java
+++ step5/page210num8.java
@@ -1,5 +1,45 @@
-//public class page210num8 {
-//    public static void main(String[] args) {
-//
-//    }
-//}
+import java.util.Scanner;
+
+public class page210num8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("요일을 입력하세요. : ");
+        String day = sc.nextLine();
+
+        System.out.print(Days.valueOf(day.toUpperCase()));
+        equarDay(day.toUpperCase());
+
+    }
+
+    public static void equarDay(String day){
+        switch (day){
+            case "MONDAY" :
+                System.out.println("은 싫다");
+                break;
+            case "FRIDAY" :
+                System.out.println("은 좋다");
+                break;
+            case "SATURDAY" :
+            case "SUNDAY" :
+                System.out.println("은 최고");
+                break;
+            default:
+                System.out.println("은 그저 그렇다");
+        }
+    }
+}
+
+enum Days{
+    MONDAY("월요일"), TUESDAY("화요일"), WENDSDAY("수요일"), THUERSDAY("목요일"), FRIDAY("금요일"), SATURDAY("토요일"), SUNDAY("일요일");
+
+    private String s;
+
+    Days(String s){
+        this.s = s;
+        
+    }
+
+    public String toString(){
+        return s;
+    }
+}
